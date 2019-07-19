950d9e95c850117770d2fbb3a546cc13f9809153
Alexander Gribov <alexander.gribov@devexpress.com>
2019-07-19 12:17:39 +0300

    wip

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,6 +3,17 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.*
 
 version = "2018.2"
 project{
+    vcsRoot {
+        GitVcsRoot{
+            d("KotlinTestVcs")
+            name = "kotlin_test_vcs"
+            url = "https://github.com/GribovAlexander/test-slave.git"
+            authMethod = password {
+                userName = "GribovAlexander"
+                password = "credentialsJSON:9fe5000e-f6ce-4c30-8a3e-b95501181cec"
+            }
+        }
+    }
     buildType {
         id ("HelloWorld")
         name="HelloW"
@@ -10,6 +21,9 @@ project{
             script {
                 scriptContent="echo HelloW"
             }
+        }
+        vcs {
+            add(RelativeId("KotlinTestVcs"))
         }
     }
     buildType {
